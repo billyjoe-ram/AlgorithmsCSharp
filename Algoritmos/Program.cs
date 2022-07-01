66e7ef875441bc45a4153d6d4af134a71999e134
Billy Joe <billyjoersantos@outlook.com>
2022-06-30 21:53:10 -0300

    feature(algorithms): Sorting Algorithms with C# - Quick
    Implementing Quick Sort algorithm

--- Algoritmos/Program.cs
+++ Algoritmos/Program.cs
@@ -5,7 +5,7 @@ Console.WriteLine("Hello, World!\nWelcome to Sorting Algorithms 101!\n");
 
 int[] toSortArray = new int[]
 {
-    23, 48, 12, 99, 1, 2, 1,  0, 24
+    23, 48, 12, 99, 1, 2, 1, 0, 24
 };
 
 Console.WriteLine("Array before sorting");
@@ -17,5 +17,7 @@ Console.WriteLine("\nArray after sorting");
 Console.WriteLine(string.Join(", ", new BubbleSort().Sort(toSortArray)));
 
 Console.WriteLine(string.Join(", ", new SelectionSort().Sort(toSortArray)));
+
+Console.WriteLine(string.Join(", ", new QuickSort().Sort(toSortArray)));
 
 Console.ReadLine();
---
+++ Algoritmos/Sorts/QuickSort.cs
@@ -0,0 +1,67 @@
+﻿namespace Algoritmos.Sorts
+{
+    internal class QuickSort : ISortAlgorithm
+    {
+        /// <summary>
+        ///     A sorting method that set a pivot
+        ///     in the collection and QUICK SORTS
+        ///     smaller values to the left of this
+        ///     pivot and greater values to the right
+        ///     of the pivot, recursively replacing
+        ///     this pivot to sort every section.
+        ///     
+        /// </summary>
+        /// <param name="collection"></param>
+        /// <returns></returns>
+        public int[] Sort(int[] collection)
+        {
+            var sortedCollection = new int[collection.Length];
+
+            collection.CopyTo(sortedCollection, 0);
+
+            RecursiveSort(sortedCollection, 0, sortedCollection.Length - 1);
+
+            return sortedCollection;
+        }
+
+        private void RecursiveSort(int[] collection, int startIndex, int endIndex)
+        {
+            if (startIndex < endIndex)
+            {
+                // Index which the sorting stopped after left and right partition
+                int pivotIndex = Partition(collection, startIndex, endIndex);
+
+                // Sort left partition
+                RecursiveSort(collection, startIndex, pivotIndex - 1);
+                // Sort right partitin
+                RecursiveSort(collection, pivotIndex + 1, endIndex);
+            }
+        }
+
+        private int Partition(int[] collection, int startIndex, int endIndex)
+        {
+            int tempSwapVar;
+            int valuesBetween = startIndex;
+
+            for (int i = startIndex + 1; i <= endIndex; i++)
+            {
+
+                if (collection[i] < collection[startIndex])
+                {
+                    valuesBetween++;
+
+                    tempSwapVar = collection[i];
+                    collection[i] = collection[valuesBetween];
+                    collection[valuesBetween] = tempSwapVar;
+                }
+            }
+
+            tempSwapVar = collection[startIndex];
+            collection[startIndex] = collection[valuesBetween];
+            collection[valuesBetween] = tempSwapVar;
+
+            return valuesBetween;
+        }
+
+    }
+}
