630dcb7f1e6ef766e36823d2c90e24c56357f56a
Billy Joe <billyjoersantos@outlook.com>
2022-07-06 20:42:33 -0300

    test(algorithms): Algorithms with C#
    Adding test projects and two test classes for searchs and sorts

---
+++ Algoritmos.Tests/SearchsTests.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace Algoritmos.Tests
+{
+    public class SearchsTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ Algoritmos.Tests/SortsTests.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace Algoritmos.Tests
+{
+    public class SortsTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
