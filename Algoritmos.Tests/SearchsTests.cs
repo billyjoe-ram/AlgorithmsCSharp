1ed130435c67894fb30e7b72bf83f8aabeeff325
Billy Joe <billyjoersantos@outlook.com>
2022-07-06 22:13:11 -0300

    test(algorithms): Algorithms with C#
    Adding unit tests for the search algorithms implemented

--- Algoritmos.Tests/SearchsTests.cs
+++ Algoritmos.Tests/SearchsTests.cs
@@ -1,13 +1,78 @@
 ﻿using Xunit;
+using System;
+
+using Algoritmos.Sorts;
+using Algoritmos.Searchs;
 
 namespace Algoritmos.Tests
 {
     public class SearchsTests
     {
-        [Fact]
-        public void Test1()
-        {
+        private readonly int _chosenNumber = 100;
+        private readonly int[] _numbersArray = new int[10];
 
+        public SearchsTests()
+        {
+            var random = new Random();
+
+            for (int i = 0; i < 9; i++)
+            {
+                _numbersArray[i] = random.Next(0, 100);
+            }
+
+            _numbersArray[^1] = _chosenNumber;
+        }
+
+        [Trait("Searchs", "Linear")]
+        [Fact(DisplayName = "Linear search finding")]
+        public void TestLinearSearch()
+        {
+            var searchMethod = new BinarySearch();
+
+            var sortedArray = new QuickSort().Sort(_numbersArray);
+
+            int index = searchMethod.Search(_chosenNumber, sortedArray);
+
+            Assert.NotEqual(9, index);
+        }
+
+        [Trait("Searchs", "Linear")]
+        [Fact(DisplayName = "Linear search not finding")]
+        public void TestLinearSearchElementNotFound()
+        {
+            var searchMethod = new BinarySearch();
+
+            var sortedArray = new QuickSort().Sort(_numbersArray);
+
+            int index = searchMethod.Search(101, _numbersArray);
+
+            Assert.Equal(-1, index);
+        }
+
+        [Trait("Searchs", "Binary")]
+        [Fact(DisplayName = "Binary search finding")]
+        public void TestBinarySearch()
+        {
+            var searchMethod = new BinarySearch();
+
+            var sortedArray = new QuickSort().Sort(_numbersArray);
+
+            int index = searchMethod.Search(_chosenNumber, sortedArray);
+
+            Assert.NotEqual(9, index);
+        }
+
+        [Trait("Searchs", "Binary")]
+        [Fact(DisplayName = "Binary search not finding")]
+        public void TestBinarySearchElementNotFound()
+        {
+            var searchMethod = new BinarySearch();
+
+            var sortedArray = new QuickSort().Sort(_numbersArray);
+
+            int index = searchMethod.Search(101, _numbersArray);
+
+            Assert.Equal(-1, index);
         }
     }
 }
