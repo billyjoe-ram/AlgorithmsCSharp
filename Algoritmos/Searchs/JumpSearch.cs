d13a29535c5080422efc3471752a4889460163c7
Billy Joe <billyjoersantos@outlook.com>
2022-07-07 21:52:15 -0300

    feature(algorithms): Algorithms with C# - Jump Search
    Implementing Jump Search algorithm

---
+++ Algoritmos/Searchs/JumpSearch.cs
@@ -0,0 +1,35 @@
+﻿namespace Algoritmos.Searchs
+{
+    public class JumpSearch : ISearchAlgorithm
+    {
+        public int Search(int value, int[] collection)
+        {
+            int jumpFactor = (int) Math.Floor(Math.Sqrt(collection.Length));
+            // Subtracting one to get the indexes starting in 0
+            int searchIndex = jumpFactor - 1;
+
+            int indexFound = -1;
+
+            // Jumping to a search index in front of the searched value
+            while (searchIndex < collection.Length && value > collection[searchIndex])
+            {
+                searchIndex += jumpFactor;
+            }
+
+            // Going back to search for the value from the closes index before
+            searchIndex -= jumpFactor;
+
+            for (int i = searchIndex; i < collection.Length; i++)
+            {
+                if (collection[i] != value)
+                {
+                    continue;
+                }
+
+                indexFound = i;
+            }
+
+            return indexFound;
+        }
+    }
+}
