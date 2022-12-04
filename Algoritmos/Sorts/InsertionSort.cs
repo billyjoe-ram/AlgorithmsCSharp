64e36f4a49dee75edd4460134757fc8d5611b588
Billy Joe <billyjoersantos@outlook.com>
2022-12-04 11:12:13 -0300

    feature(algorithms): Sorting Algorithms with C# - Insertion
    Implementing Insertion Sort algorithm

---
+++ Algoritmos/Sorts/InsertionSort.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Algoritmos.Sorts
+{
+    public class InsertionSort : ISortAlgorithm
+    {
+        public int[] Sort(int[] collection)
+        {
+            var sortedCollection = new int[collection.Length];
+
+            collection.CopyTo(sortedCollection, 0);
+            
+            int firstUnsortedElement, j;
+
+            for (int i = 1; i <= sortedCollection.Length - 1; i++)
+            {
+                firstUnsortedElement = sortedCollection[i];
+                j = i - 1;
+
+                while (j >= 0 && sortedCollection[j] > firstUnsortedElement)
+                {
+                    sortedCollection[j + 1] = sortedCollection[j];
+
+                    j--;
+                }
+
+                sortedCollection[j + 1] = firstUnsortedElement;
+            }            
+
+            return sortedCollection;
+        }        
+
+    }
+}
